ddd86f4132a003a5bfc22f87fa15349418655d39
wilsonjuniorwj <wilsonwj0101@gmail.com>
2018-08-03 20:58:58 -0300

    Add target_blank.js

---
+++ src/js/target_blank.js
@@ -0,0 +1,10 @@
+(function() {
+    var links = document.links;
+    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+      if (links[i].hostname != window.location.hostname) {
+        links[i].target = '_blank';
+        links[i].rel = 'noopener';
+        links[i].className += ' externalLink';
+      }
+    }
+  })();
